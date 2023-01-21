ddf524a4a448f0cf39765a49d3d1646101325324
Naelem Silva <Ellenxxy19@gmail.com>
2023-01-21 19:31:34 -0300

    finalizei arquivo js

--- script.js
+++ script.js
@@ -10,27 +10,18 @@ function add() {
   const dayExists = nlwSetup.dayExists(today)
 
   if(dayExists) {
-    alert('Dia já incluso')
+    alert('Dia já incluso ❌')
     return
   }
+  alert('Adicionado com sucesso ✔')
   nlwSetup.addDay(today)
 }
 
 function save() {
-  
+  localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
 }
 
-
-
-
-
-/*const data = {
-  run: ["01-01", "01-02"],
-  water: ["01-04", "01-05"],
-  food: ["01-01", "01-03"],
-  read: ['01-03', '01-04']
-}
-
+const data = JSON.parse(localStorage.getItem ('NLWSetup@habits')) || {}
 nlwSetup.setData(data)
-nlwSetup.load() */
+nlwSetup.load()
 
